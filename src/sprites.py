e6b05046279f5c2d31dd8e1082d8e9d5d202dee2
Eccsx <thomasmattone.perso@gmail.com>
2023-09-05 14:46:30 +0200

    add sprites doc

--- src/sprites.py
+++ src/sprites.py
@@ -13,7 +13,14 @@ INDICATOR_COLOR = (159, 100, 187)
 
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, x, y, corner=None):
+    """Sprite of board cell.
+
+    Args:
+        x (int): x-axis top-left pixel position.
+        y (int): y-axis top-left pixel position.
+    """
+
+    def __init__(self, x, y):
         super().__init__()
 
         self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
@@ -27,12 +34,15 @@ class Cell(pygame.sprite.Sprite):
 
 
 class Board(pygame.sprite.Group):
+    """Sprite group of the board."""
+
     def __init__(self):
         super().__init__()
 
         self.init_board()
 
     def init_board(self):
+        """Initiliaze board cells position."""
         for y in range(8):
             for x in range(8):
                 self.add(
@@ -44,6 +54,16 @@ class Board(pygame.sprite.Group):
 
 
 class Piece(pygame.sprite.Sprite):
+    """Sprite of a piece.
+
+    A piece can be black or white, depending on which player it belongs to.
+
+    Args:
+        row (int): row position on the board.
+        col (int): column position on the board.
+        is_black (bool): color of the piece.
+    """
+
     def __init__(self, row, col, is_black):
         super().__init__()
 
@@ -70,12 +90,15 @@ class Piece(pygame.sprite.Sprite):
 
 
 class PieceLayout(pygame.sprite.Group):
+    """Sprite group of all the pieces on the board."""
+
     def __init__(self):
         super().__init__()
 
         self.init_layout()
 
     def init_layout(self):
+        """Initiliaze starting piece layout."""
         self.add(
             Piece(3, 3, is_black=False),
             Piece(3, 4, is_black=True),
@@ -85,6 +108,16 @@ class PieceLayout(pygame.sprite.Group):
 
 
 class Indicator(pygame.sprite.Sprite):
+    """Sprite of a move indicator.
+
+    Only certain moves are legal on each player's turn, notified by indicators
+    for more convenience.
+
+    Args:
+        row (int): row position on the board.
+        col (int): column position on the board.
+    """
+
     def __init__(self, row, col):
         super().__init__()
 
@@ -112,12 +145,15 @@ class Indicator(pygame.sprite.Sprite):
 
 
 class IndicatorLayout(pygame.sprite.Group):
+    """Sprite group of all move indicators."""
+
     def __init__(self):
         super().__init__()
 
         self.init_layout()
 
     def init_layout(self):
+        """Initiliaze starting indicator layout."""
         self.add(
             Indicator(2, 3),
             Indicator(3, 2),
@@ -127,6 +163,17 @@ class IndicatorLayout(pygame.sprite.Group):
 
 
 class PhantomPiece(pygame.sprite.Sprite):
+    """Sprite of a phantom piece.
+
+    A phantom piece is used to help the player visualize his next move by
+    replacing an indicator with a semi-transparent version of his piece.
+
+    Args:
+        row (_type_): row position on the board.
+        col (_type_): color position on the board.
+        is_black (bool): color of the piece.
+    """
+
     def __init__(self, row, col, is_black):
         super().__init__()
 
