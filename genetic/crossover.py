fbff49e1bb24200b29fabe16c7979cfab71df39f
Eccsx <thomasmattone.perso@gmail.com>
2023-11-05 00:09:00 +0100

    add two-point crossover

--- genetic/crossover.py
+++ genetic/crossover.py
@@ -24,3 +24,31 @@ def one_point_crossover(
     c2 = np.append(p1[:point_index], p2[point_index:])
 
     return c1 / sum(c1), c2 / sum(c2)
+
+
+def two_point_crossover(
+    p1: np.ndarray, p2: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
+    """Performs two-point crossover on two parent chromosomes and retuns two new child
+    chromosomes.
+
+    New chromosomes are normalized to fit requirements.
+
+    Args:
+        p1 (np.ndarray): first parent chromosome.
+        p2 (np.ndarray): second parent chromosome.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: child chromosomes.
+    """
+    rng = np.random.default_rng()
+
+    point_indices = rng.choice(range(1, len(p1)), 2, replace=False)
+    point_indices.sort()
+
+    i1, i2 = point_indices
+
+    c1 = np.concatenate((p1[:i1], p2[i1:i2], p1[i2:]))
+    c2 = np.concatenate((p2[:i1], p1[i1:i2], p2[i2:]))
+
+    return c1 / sum(c1), c2 / sum(c2)
--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from genetic.crossover import one_point_crossover
+from genetic.crossover import one_point_crossover, two_point_crossover
 
 
 def test_one_point_crossover(monkeypatch):
@@ -41,6 +41,49 @@ def test_one_point_crossover(monkeypatch):
         (
             np.array([0, 0.2, 0.2, 0.2, 0.2, 0.2]),
             np.array([1, 0, 0, 0, 0, 0]),
+        ),
+        atol=1e-2,
+    )
+
+
+def test_two_point_crossover(monkeypatch):
+    values = np.array([[2, 3], [2, 4], [1, 2]])
+    i = 0
+
+    class Mock_default_rng:
+        def choice(self, a, b, replace):
+            nonlocal i
+            i += 1
+            return values[i - 1]
+
+    monkeypatch.setattr(np.random, "default_rng", Mock_default_rng)
+
+    p1 = np.array([1, 1, 1, 1, 1, 1])
+    p2 = np.array([0, 0, 0, 0, 0, 0])
+
+    assert np.allclose(
+        two_point_crossover(p1, p2),
+        (
+            np.array([0.2, 0.2, 0, 0.2, 0.2, 0.2]),
+            np.array([0, 0, 1, 0, 0, 0]),
+        ),
+        atol=1e-2,
+    )
+
+    assert np.allclose(
+        two_point_crossover(p1, p2),
+        (
+            np.array([0.25, 0.25, 0, 0, 0.25, 0.25]),
+            np.array([0, 0, 0.5, 0.5, 0, 0]),
+        ),
+        atol=1e-2,
+    )
+
+    assert np.allclose(
+        two_point_crossover(p1, p2),
+        (
+            np.array([0.2, 0, 0.2, 0.2, 0.2, 0.2]),
+            np.array([0, 1, 0, 0, 0, 0]),
         ),
         atol=1e-2,
     )
