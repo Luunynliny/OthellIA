52d40e051cf8c48180270acf36304f3a265e57dc
Eccsx <thomasmattone.perso@gmail.com>
2023-11-05 00:50:52 +0100

    add uniform crossover

--- genetic/crossover.py
+++ genetic/crossover.py
@@ -43,12 +43,44 @@ def two_point_crossover(
     """
     rng = np.random.default_rng()
 
-    point_indices = rng.choice(range(1, len(p1)), 2, replace=False)
+    point_indices = rng.choice(range(1, len(p1)), size=2, replace=False)
     point_indices.sort()
 
     i1, i2 = point_indices
 
     c1 = np.concatenate((p1[:i1], p2[i1:i2], p1[i2:]))
     c2 = np.concatenate((p2[:i1], p1[i1:i2], p2[i2:]))
+
+    return c1 / sum(c1), c2 / sum(c2)
+
+
+def uniform_crossover(
+    p1: np.ndarray, p2: np.ndarray, p_swap: int
+) -> tuple[np.ndarray, np.ndarray]:
+    """Performs uniform crossover on two parent chromosomes and retuns two new child
+    chromosomes.
+
+    New chromosomes are normalized to fit requirements.
+
+    Args:
+        p1 (np.ndarray): first parent chromosome.
+        p2 (np.ndarray): second parent chromosome.
+        p_swap(int): probability of gene swapping.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: child chromosomes.
+    """
+    rng = np.random.default_rng()
+
+    binary_mask = rng.choice([0, 1], size=len(p1), p=[1 - p_swap, p_swap])
+
+    c1 = p1.copy()
+    c2 = p2.copy()
+
+    for i, b in enumerate(binary_mask):
+        if b == 0:
+            continue
+
+        c1[i], c2[i] = c2[i], c1[i]
 
     return c1 / sum(c1), c2 / sum(c2)
--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-from genetic.crossover import one_point_crossover, two_point_crossover
+from genetic.crossover import (
+    one_point_crossover,
+    two_point_crossover,
+    uniform_crossover,
+)
 
 
 def test_one_point_crossover(monkeypatch):
@@ -84,6 +88,51 @@ def test_two_point_crossover(monkeypatch):
         (
             np.array([0.2, 0, 0.2, 0.2, 0.2, 0.2]),
             np.array([0, 1, 0, 0, 0, 0]),
+        ),
+        atol=1e-2,
+    )
+
+
+def test_uniform_crossover(monkeypatch):
+    values = np.array(
+        [[1, 1, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0], [0, 0, 0, 1, 0, 0]]
+    )
+    i = 0
+
+    class Mock_default_rng:
+        def choice(self, a, size, p):
+            nonlocal i
+            i += 1
+            return values[i - 1]
+
+    monkeypatch.setattr(np.random, "default_rng", Mock_default_rng)
+
+    p1 = np.array([1, 1, 1, 1, 1, 1])
+    p2 = np.array([0, 0, 0, 0, 0, 0])
+
+    assert np.allclose(
+        uniform_crossover(p1, p2, 0.5),
+        (
+            np.array([0, 0, 0.5, 0.5, 0, 0]),
+            np.array([0.25, 0.25, 0, 0, 0.25, 0.25]),
+        ),
+        atol=1e-2,
+    )
+
+    assert np.allclose(
+        uniform_crossover(p1, p2, 0.8),
+        (
+            np.array([0, 0, 0, 0, 0.5, 0.5]),
+            np.array([0.25, 0.25, 0.25, 0.25, 0, 0]),
+        ),
+        atol=1e-2,
+    )
+
+    assert np.allclose(
+        uniform_crossover(p1, p2, 0.1),
+        (
+            np.array([0.2, 0.2, 0.2, 0, 0.2, 0.2]),
+            np.array([0, 0, 0, 1, 0, 0]),
         ),
         atol=1e-2,
     )
