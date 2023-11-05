7ca162f14099f75c90f71bf486335ae9b04e59b2
Eccsx <thomasmattone.perso@gmail.com>
2023-11-05 17:56:48 +0100

    add scramble mutation

--- genetic/mutate.py
+++ genetic/mutate.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def swap(chromosome: np.ndarray) -> np.ndarray:
-    """Perform swap mutation and returns the new chromosome state.
+    """Performs swap mutation and returns the new chromosome state.
 
     Args:
         chromosome (np.ndarray): a chromosome.
@@ -20,3 +20,30 @@ def swap(chromosome: np.ndarray) -> np.ndarray:
     mutated[i1], mutated[i2] = mutated[i2], mutated[i1]
 
     return mutated
+
+
+def scramble(chromosome: np.ndarray) -> np.ndarray:
+    """Performs scramble mutation and returns the new chromosome state.
+
+    Args:
+        chromosome (np.ndarray): a chromosome.
+
+    Returns:
+        np.ndarray: mutated chromosome.
+    """
+    rng = np.random.default_rng()
+
+    to_scramble_indices = rng.choice(
+        range(len(chromosome)), size=2, replace=False
+    )
+    to_scramble_indices.sort()
+
+    i1, i2 = to_scramble_indices
+
+    return np.concatenate(
+        [
+            chromosome[:i1],
+            rng.shuffle(chromosome[i1 : i2 + 1]),
+            chromosome[i2 + 1 :],
+        ]
+    )
--- tests/test_mutate.py
+++ tests/test_mutate.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from genetic.mutate import swap
+from genetic.mutate import scramble, swap
 
 
 def test_swap(monkeypatch):
@@ -20,3 +20,25 @@ def test_swap(monkeypatch):
     assert np.array_equal(swap(chromosome), np.array([1, 6, 3, 4, 5, 2]))
     assert np.array_equal(swap(chromosome), np.array([1, 2, 4, 3, 5, 6]))
     assert np.array_equal(swap(chromosome), np.array([5, 2, 3, 4, 1, 6]))
+
+
+def test_scramble(monkeypatch):
+    values = np.array([[1, 5], [2, 3], [0, 4]])
+    i = 0
+
+    class Mock_default_rng:
+        def choice(self, a, size, replace):
+            nonlocal i
+            i += 1
+            return values[i - 1]
+
+        def shuffle(self, a):
+            return a[::-1]
+
+    monkeypatch.setattr(np.random, "default_rng", Mock_default_rng)
+
+    chromosome = np.array([1, 2, 3, 4, 5, 6])
+
+    assert np.array_equal(scramble(chromosome), np.array([1, 6, 5, 4, 3, 2]))
+    assert np.array_equal(scramble(chromosome), np.array([1, 2, 4, 3, 5, 6]))
+    assert np.array_equal(scramble(chromosome), np.array([5, 4, 3, 2, 1, 6]))
